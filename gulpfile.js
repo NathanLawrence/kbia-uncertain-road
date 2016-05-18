a5c2083c76ae3d853c67f335cc55485833c5d6ae
Nathan Lawrence <lawrencen@missouri.edu>
2016-05-18 15:12:15 -0500

    Added a bunch of neato CSS and Javascript styling.

--- gulpfile.js
+++ gulpfile.js
@@ -48,6 +48,6 @@ gulp.task('watch',function(){
 	gutil.log('Gulp will say that this task has finished, but don\'t believe its dirty lies.');
 	gutil.log('Hit \^c to actually exit watch mode.');
 	gulp.watch('components/less/**/*.less',['less']);
-	gulp.watch('components/js/**/*.js',['js']);
+	gulp.watch('components/**/*.js',['js']);
 	gulp.watch('components/**/*.html',['html']);
 });
