a1abdcd84caa241430d89e5920ba8437bba6ee26
Nathan Lawrence <lawrencen@missouri.edu>
2016-05-31 11:05:43 -0500

    Added interactive ToC.

--- build/js/ur-styling.js
+++ build/js/ur-styling.js
@@ -1,10 +1,33 @@
 $(document).ready(function(){
 	var vpHeight = $(window).height();
 
+	var initialScroll = $(window).scrollTop();
+
+	var williamTop = $('#william').offset().top;
+
 	$('.title-card').css('height',vpHeight);
 
 	// $('.section-head').css('height', vpHeight - 100);
 
+
+
+	// Add Table of Contents to Page
+	var tocDetect = function(scrollPos){
+
+		vpWidth = $(window).width();
+
+		if (vpWidth >= 750) {
+			if(scrollPos >= williamTop + 600){
+				$('.internal-nav').removeClass('hidden');
+				$('.internal-nav').fadeIn();
+				$('.top-after').css('margin-top',$('.internal-nav').height() + 50);
+			}
+			else{
+				$('.internal-nav').addClass('hidden');
+				$('.top-after').css('margin-top', 50);
+			}
+		}
+	};
 
 	var winHeight = $(window).height(), 
       docHeight = $(document).height(),
@@ -18,5 +41,9 @@ $(document).ready(function(){
 	$(document).on('scroll', function(){
 	   value = $(window).scrollTop();
 	   progressBar.attr('value', value);
+
+	   // Call table of contents detection
+	   tocDetect(value);
+
 	});
 });
